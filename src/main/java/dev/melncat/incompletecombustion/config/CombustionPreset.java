d861e38447666c2c5c61b054a2ea0d81aa737dbf
MelnCat <melncatuwu@gmail.com>
2024-07-02 14:21:33 -0700

    more preset

--- src/main/java/dev/melncat/incompletecombustion/config/CombustionPreset.java
+++ src/main/java/dev/melncat/incompletecombustion/config/CombustionPreset.java
@@ -12,12 +12,16 @@ public enum CombustionPreset {
 		m.addTagConversion(Tag.WOODEN_STAIRS, Material.DARK_OAK_STAIRS);
 		m.addTagConversion(Tag.WOODEN_SLABS, Material.DARK_OAK_SLAB);
 		m.addTagConversion(Tag.WOODEN_FENCES, Material.DARK_OAK_FENCE);
+		m.addTagConversion(Tag.WOODEN_PRESSURE_PLATES, Material.DARK_OAK_PRESSURE_PLATE);
+		m.addTagConversion(Tag.WOODEN_BUTTONS, Material.DARK_OAK_BUTTON);
 	}),
 	NETHER_BRICK(m -> {
 		m.addTagConversion(Tag.PLANKS, Material.NETHER_BRICKS);
 		m.addTagConversion(Tag.WOODEN_STAIRS, Material.NETHER_BRICK_STAIRS);
 		m.addTagConversion(Tag.WOODEN_SLABS, Material.NETHER_BRICK_SLAB);
 		m.addTagConversion(Tag.WOODEN_FENCES, Material.NETHER_BRICK_FENCE);
+		m.addTagConversion(Tag.WOODEN_PRESSURE_PLATES, Material.DARK_OAK_PRESSURE_PLATE);
+		m.addTagConversion(Tag.WOODEN_BUTTONS, Material.DARK_OAK_BUTTON);
 	});
 	
 	private final Consumer<CombustionManager> callback;

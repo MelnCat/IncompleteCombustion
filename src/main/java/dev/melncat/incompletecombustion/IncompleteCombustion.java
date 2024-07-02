930cbaf8c544f88c5423cc9028105dbb880b9407
MelnCat <melncatuwu@gmail.com>
2024-07-02 14:03:42 -0700

    actually load the config

--- src/main/java/dev/melncat/incompletecombustion/IncompleteCombustion.java
+++ src/main/java/dev/melncat/incompletecombustion/IncompleteCombustion.java
@@ -1,5 +1,6 @@
 package dev.melncat.incompletecombustion;
 
+import dev.melncat.incompletecombustion.config.CombustionConfig;
 import dev.melncat.incompletecombustion.listener.BurnListener;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -8,17 +9,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class IncompleteCombustion extends JavaPlugin {
     private final CombustionManager combustionManager = new CombustionManager();
+    private CombustionConfig config;
 
     @Override
     public void onEnable() {
-        
-        
+        saveDefaultConfig();
+        loadConfig();
         Bukkit.getPluginManager().registerEvents(new BurnListener(this), this);
     }
 
     @Override
     public void onDisable() {
         combustionManager.clearConversions();
+    }
+    
+    public void loadConfig() {
+        reloadConfig();
+        config = new CombustionConfig(getConfig());
+        combustionManager.applyConfig(config);
     }
     
     public CombustionManager getCombustionManager() {
